91d644c6c4ea91935766787c564b783fe47cb22b
EbediMeleck <ituzeebedi12@gmail.com>
2025-01-27 05:12:40 -0800

    updated files

--- singleLinkedLIst.c
+++ singleLinkedLIst.c
@@ -11,7 +11,17 @@ int main(){
 	struct node *head =NULL;
 	head = (struct node*)malloc(sizeof(struct node));
     head->data = 34;
-    printf("%d",head->data);
-	
+    head->link=NULL;
+    printf("%d",head->data);	
+    struct node *prt = NULL;
+    prt=malloc(sizeof(struct node ));
+    prt->data= 35;
+    prt->link=NULL;
+    prt=head;
+    
+    while(prt!=NULL){
+    	printf("%d\n",prt->data);
+    	prt=prt->link;
+	}
 	return 0;
 }
